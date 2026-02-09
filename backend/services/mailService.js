5e4fe55e26306205f1641c1867c6dee751dbbd2d
Satyam Verma <satyam.verma.og27@gmail.com>
2026-02-09 08:47:38 +0530

    fix email service

--- backend/services/mailService.js
+++ backend/services/mailService.js
@@ -1,14 +1,16 @@
 import nodemailer from "nodemailer";
 
-export const sendOTPEmail = async (email, otp) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // false for TLS
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
+export const sendOTPEmail = async (email, otp) => {
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to: email,
